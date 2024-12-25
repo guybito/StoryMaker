f2eec6a09f2ae1587b9fcb620a4a546327e08d1f
IdoDai <127848347+IdoDai@users.noreply.github.com>
2024-12-25 18:54:29 +0200

    Update plotter.py

--- Plotto/plotter.py
+++ Plotto/plotter.py
@@ -26,7 +26,7 @@ class Plotter:
         self.lead_ins = 0
         self.carry_ons = 0
         self.curr_name_mapping = {}
-
+        self.transforms_dict = {}
 
     def _picker(self, items: List, label: Optional[str] = None) -> Optional[str]:
         """Randomly pick an item from a list."""
@@ -88,7 +88,6 @@ class Plotter:
         A_Clause = random_clause(self.plotto['A_Clauses'])
         B_Clause = random_clause(self.plotto['B_Clauses'])
         logging.debug(f"B_Clause: {B_Clause}")
-
         C_Clause = random_clause(self.plotto['C_Clauses'])
         root_id = "root"
         conflict = self.plotto['conflicts'][random_clause(B_Clause['nodes'])]
@@ -97,11 +96,7 @@ class Plotter:
         self.root = conflict
         plot = self._expand(conflict, root_transform, {"leadIns": self.lead_ins, "carryOns": self.carry_ons},
                             expand_id=root_id).replace('*', '')
-        # print("plot = ",plot)
-        # plot = self._apply_names(plot, actors)
-        # logging.debug(f"actors are: {actors}")
         plot = self.fix_pronouns_contextually(plot, self.curr_name_mapping)
-
         return {
                 "a clause": f"{A_Clause}",
                 "group": B_Clause["group"],
@@ -111,39 +106,18 @@ class Plotter:
                 "plot": f"{B_Clause}\n\n{plot}\n\n{C_Clause}".strip(),
                 "c clause": f"{C_Clause}"
                 }
-
-
+    #### need to check the purpose of this function ####
     def _get_gender_transform(self):
         """Create a mapping to flip genders."""
         transform = {f"A-{i}": f"B-{i}" for i in range(10)}
         transform.update({f"B-{i}": f"A-{i}" for i in range(10)})
         return transform
 
-    # def _apply_names(self, text: str, actors: List[dict]) -> str:
-    #     """Replace character symbols with names."""
-    #     male_names = self.names_data["male_names"]
-    #     female_names = self.names_data["female_names"]
-    #     name_cache = {}
-    #     rg = self._transform_to_regex(self.plotto['characters'])
-    #     # print("rg = ",rg)
-    #     if rg:
-    #         def replacer(match):
-    #             symbol = match.group(0)
-    #             # print("symbol = ",symbol)
-    #             if symbol in name_cache:
-    #                 return name_cache[symbol]
-    #             description = self.plotto['characters'].get(symbol, '')
-    #             name = random_name(symbol, description, self.gender_map[symbol], male_names, female_names)
-    #             name_cache[symbol] = name
-    #             actors.append({"symbol": symbol, "name": name, "description": description})
-    #             return name
-    #
-    #         return re.sub(rg, replacer, text)
-    #     return text
-
     #re-write the function
-    def _apply_names(self, text: str, actors: List[dict]) -> str:
+    def _apply_names(self, text: str) -> str:
         """Replace character symbols with names, treating suffixes as distinct characters."""
+        if isinstance(text, list):
+            text = text[0]
         male_names = self.names_data["male_names"]
         female_names = self.names_data["female_names"]
         rg = self._transform_to_regex(self.plotto['characters'])
@@ -154,7 +128,7 @@ class Plotter:
                     return self.curr_name_mapping[symbol]
                 description = self.plotto['characters'].get(symbol, '')
                 gender = self.gender_map.get(symbol, 'any')
-                name = random_name(symbol, description, gender, male_names, female_names)
+                name = random_name(symbol, gender, male_names, female_names)
                 self.curr_name_mapping[symbol] = name
                 # actors.append({"symbol": symbol, "name": name, "description": description})
                 return name
@@ -180,14 +154,44 @@ class Plotter:
     #     pattern = r'\b(?:' + '|'.join(map(re.escape, keys)) + r')\b(?![a-z])'
     #     return re.compile(pattern)
 
+    def fill_sentences(self, sentence_id):
+            full_sentence = ''
+            new_description = self.plotto['conflicts'][sentence_id]['description']
+            if isinstance(new_description, list):
+                for s_id in new_description:
+                    full_sentence += ', ' + self.fill_sentences(s_id)
+                return full_sentence
+            else:
+                return new_description
+
+    def tfm_characters(self, tfm, sentence):
+        print(f'characters are: {self.curr_name_mapping.items()}')
+        for org, new in tfm.items():
+            self.known_symbols(new)
+            print(f'tfm items: {tfm.items()}')
+            original_character = self.curr_name_mapping.get(org)
+            new_character = self.curr_name_mapping.get(new)
+            print(f'original character: {original_character}')
+            print(f'new character: {new_character}')
+            sentence = sentence.replace(original_character, new_character)
+        return sentence
+
+    def known_symbols(self, symbol):
+        if self.curr_name_mapping.get(symbol) is None:
+            if self.gender_map.get(symbol):
+                gender = self.gender_map.get(symbol)
+                male_names = self.names_data["male_names"]
+                female_names = self.names_data["female_names"]
+                name = random_name(symbol, gender, male_names, female_names)
+                self.curr_name_mapping[symbol] = name
+
+
     def _expand(self, item, transform, ctx, start=None, end=None, expand_id=""):
         """Expand an item recursively and track meaningful IDs in order."""
         self.expand_ids.append(expand_id)
         ret = []
-
         if not item:
             return f'NULL [{expand_id}]'
-
         # Add the main conflict description if it hasn't been added yet
         if isinstance(item, dict) and "conflictid" in item:
             sentence_id = item.get("conflictid")
@@ -195,16 +199,18 @@ class Plotter:
                 if ctx["leadIns"] == 0 and len(self.ordered_sentences) == self.lead_ins + 1:
                     self.root = item
                     self.ordered_sentences = list(reversed(self.ordered_sentences))
-                self.ordered_sentences.append(sentence_id)  # Track the sentence order
+                self.ordered_sentences.append(sentence_id)
+                # Track the sentence order
+                print("description")
+                print(item['description'])
                 # if isinstance(item['description'], list):
-                #     new_description = item['description'][0]
-                #     for sentence_id in item['description'][1]:
-                #         print(type(sentence_id))
-                #         print(self.plotto['conflicts'][sentence_id]['description'])
-                #         print('---------------')
-                #         new_description += ', ' + self.plotto['conflicts'][sentence_id]['description']
-                #         ret.append(f"{item['description']} [{sentence_id}]")
-                # else:
+                #     rest_of_sentence = ''
+                #     curr_sentence_description = item['description'][0]
+                #     for s_id in item['description'][1]:
+                #         rest_of_sentence += self.fill_sentences(s_id)
+                #     item["description"] = curr_sentence_description + rest_of_sentence
+                # item['description'] = self._apply_names(item['description'])
+                item['description'] = self._apply_names(item['description'])
                 ret.append(f"{item['description']} [{sentence_id}]")
                 logging.debug(f"Added main conflict to plot: {sentence_id}")
 
@@ -214,9 +220,8 @@ class Plotter:
             logging.debug(f"Processing lead-in for main conflict: {item.get('conflictid', 'N/A')}")
             lead_in_result = self._expand(item["leadIns"], None, ctx, expand_id=lead_in_id)
             # Apply names to the lead-in result
-            lead_in_result = self._apply_names(lead_in_result, [])
+            lead_in_result = self._apply_names(lead_in_result)
             ret.append(lead_in_result)
-
         # Handle carry-ons
         if ctx.get("carryOns", 0) > 0 and "carryOns" in item:
             ctx["carryOns"] -= 1
@@ -224,26 +229,28 @@ class Plotter:
             logging.debug(f"Processing carry-on for main conflict: {item.get('conflictid', 'N/A')}")
             carryon = self._expand(self.root["carryOns"], transform, ctx, expand_id=carryon_id)
             # Apply names to the carry-on result
-            carryon = self._apply_names(carryon, [])
+            carryon = self._apply_names(carryon)
             ret.append(carryon)
-
         # Expand sub-items
         if isinstance(item, str):
             logging.debug(f"Expanding conflict: {item}")
             expanded_item = self._expand(self.plotto["conflicts"].get(item, None), None, ctx,
                                          expand_id=f"{expand_id}-str")
             # Apply names to the expanded string item
-            expanded_item = self._apply_names(expanded_item, [])
+            expanded_item = self._apply_names(expanded_item)
             ret.append(expanded_item)
         elif isinstance(item, list):
             logging.debug(f"Expanding conflict list: {item}")
             expanded_list = self._expand(self._picker(item, "plot option"), None, ctx, expand_id=f"{expand_id}-list")
             # Apply names to the expanded list item
-            expanded_list = self._apply_names(expanded_list, [])
+            expanded_list = self._apply_names(expanded_list)
             ret.append(expanded_list)
         elif "v" in item:
-            logging.debug(f"Expanding conflict v: {item}")
+            if isinstance(item['v'], str):
+                if self.transforms_dict.get(item['v']) is None and item.get('tfm') and self.gender_map.get(list(item.get('tfm').keys())[0]):
+                    self.transforms_dict[item['v']] = item.get("tfm")
             if item.get("start") or item.get("end"):
+                logging.debug(f"Expanding conflict v with start or end options: {item}")
                 expanded_v = self._expand(
                     self.plotto["conflicts"].get(item["v"], None),
                     item.get("tfm"),
@@ -253,34 +260,31 @@ class Plotter:
                     expand_id=f"{expand_id}-vstartend"
                 )
                 # Apply names to the expanded v item
-                expanded_v = self._apply_names(expanded_v, [])
+                if item.get("tfm"):
+                    expanded_v = self.tfm_characters(item.get("tfm"), expanded_v)
+                expanded_v = self._apply_names(expanded_v)
                 ret.append(expanded_v)
             elif item.get("op") == "+":
+                logging.debug(f"Expanding conflict v with chaining: {item}")
                 for sub in item["v"]:
                     expanded_sub = self._expand(sub, item.get("tfm"), ctx, expand_id=f"{expand_id}-vop")
                     # Apply names to each sub-item in the list
-                    expanded_sub = self._apply_names(expanded_sub, [])
+                    if item.get("tfm"):
+                        expanded_sub = self.tfm_characters(item.get("tfm"), expanded_sub)
+                    expanded_sub = self._apply_names(expanded_sub)
                     ret.append(expanded_sub)
             else:
+                logging.debug(f"Expanding conflict v: {item}")
                 expanded_v = self._expand(item["v"], item.get("tfm"), ctx, expand_id=f"{expand_id}-v")
                 # Apply names to the expanded v item
-                expanded_v = self._apply_names(expanded_v, [])
+                if item.get("tfm"):
+                        expanded_v = self.tfm_characters(item.get("tfm"), expanded_v)
+                expanded_v = self._apply_names(expanded_v)
                 ret.append(expanded_v)
-
         # Combine results
         result = "\n\n".join(ret).strip()
-        if transform:
-            words = result.split()  # פיצול הטקסט למילים
-            updated_words = [transform.get(word, word) for word in words]  # החלפה לפי מילון
-            result = ' '.join(updated_words)
-
-
-            # rg = self._transform_to_regex(transform)
-            # if rg:
-            #     result = rg.sub(lambda m: transform.get(m.group(0), m.group(0)), result)
-
         # Apply names to the final result
-        result = self._apply_names(result, [])
+        result = self._apply_names(result)
         return result
 
     def fix_pronouns_contextually(self, text: str, actors: dict) -> str:
